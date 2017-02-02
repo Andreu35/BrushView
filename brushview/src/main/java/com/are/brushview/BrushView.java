75fdb97a0d45a61fcefb8850200868394610ac04
AndreuRuiz <andreu.chan@gmail.com>
2017-02-02 08:47:52 +0100

    Initial commit

--- brushview/src/main/java/com/are/brushview/BrushView.java
+++ brushview/src/main/java/com/are/brushview/BrushView.java
@@ -1,5 +1,5 @@
 /**
- *   Copyright (C) 2017 ARuizE
+ *   Copyright (C) 2017 Andreu35
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
-*/
+ */
 
 package com.are.brushview;
 
